7c2de10eaa8d18f4f512a96d004249b9e9602054
nanshakov <nanshakov@gmail.com>
2018-04-16 17:12:26 +0300

    Add templace

---
+++ apps/sm/src/sm_users.erl
@@ -0,0 +1,19 @@
+%%%-------------------------------------------------------------------
+%%% @author anshakov
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 16. Апр. 2018 17:00
+%%%-------------------------------------------------------------------
+-module(sm_users).
+-author("anshakov").
+
+%% API
+-export([]).
+
+get_all() -> ok.
+
+registration(Uid, Name, Ip) -> ok.
+
+get(Uid) -> ok.
